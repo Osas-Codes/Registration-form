b16c813ba3dcf0451a7e78b3c1eb31ed0cd94640
Endurance Osamwonyi <103249719+Osas-Codes@users.noreply.github.com>
2022-10-15 03:06:52 +0100

    Add files via upload

---
+++ user_data.php
@@ -0,0 +1,21 @@
+<?php
+print_r($_POST);
+
+
+if (isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $birthday = $_POST['birthday'];
+  $gender = $_POST['gender'];
+  $country = $_POST['country'];
+
+  $form_data = [$name, $email, $birthday, $gender, $country];
+
+  // $userdata = "./userdata.csv";
+  $handle = fopen("userdata.csv", "a");
+  fputcsv($handle, $form_data);
+}else {
+  echo "No data";
+}
+
+?>
